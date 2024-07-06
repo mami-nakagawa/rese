655a4792e61c8ddb8037ba5a62a8d69d86a0dd74
nakagawa mami <mya.a6a@gmail.com>
2024-07-06 19:02:27 +0900

    レビューダミーデータの作成

---
+++ src/database/factories/ReviewFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Review;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    protected $model = Review::class;
+
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->numberBetween(4, 103),
+            'shop_id' => $this->faker->numberBetween(1, 20),
+            'star' => $this->faker->numberBetween(1, 5),
+            'comment' => $this->faker-realText(),
+        ];
+    }
+}
