edc0ad53f5b2dd6d49b248dd6558034f27e2afc5
nakagawa mami <mya.a6a@gmail.com>
2024-07-14 03:15:26 +0900

    ダミーデータの修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,8 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(ShopsTableSeeder::class);
-        // User::factory(100)->create();
-        Review::factory(200)->create();
+        $this->call(GenresTableSeeder::class);
+        $this->call(ShopsTableSeeder::class);
+        User::factory(100)->create();
+        $this->call(AreasTableSeeder::class);
+        Review::factory(100)->create();
     }
 }
