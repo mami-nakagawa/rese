97993a7eb3cf172a4f9ecac0abbdf7c4c3e1112c
nakagawa mami <mya.a6a@gmail.com>
2024-07-19 00:51:51 +0900

    権限別管理画面の作成

---
+++ src/app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Area;
+use App\Models\Genre;
+use App\Models\Shop;
+use App\Models\Reservation;
+use Auth;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller
+{
+    //
+}
---
+++ src/resources/views/editor/done.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/done.css')}}">
+@endsection
+
+@section('content')
+<div class="done__inner">
+    <div class="done__card">
+        <p class="done__message">店舗情報を登録しました</p>
+        <a class="back__btn" href="/">戻る</a>
+    </div>
+</div>
+@endsection
