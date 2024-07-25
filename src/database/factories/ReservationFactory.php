e38dba369b9faca10c6399be298ace2676dcd4e5
nakagawa mami <mya.a6a@gmail.com>
2024-07-25 22:13:10 +0900

    予約情報のダミーデータ作成

---
+++ src/database/factories/ReservationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Reservation;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ReservationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    protected $model = Reservation::class;
+
+    public function definition()
+    {
+        return [
+            'user_id' => $this->faker->numberBetween(1, 100),
+            'shop_id' => $this->faker->numberBetween(1, 20),
+            'date' => $this->faker->dateTimeBetween('-1weeks', '3months')->format('Y-m-d'),
+            'time' => $this->faker->randomElement(['17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00']),
+            'number' => $this->faker->numberBetween(1, 5),
+        ];
+    }
+}
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Review;
+use App\Models\Reservation;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
         $this->call(GenresTableSeeder::class);
         $this->call(ShopsTableSeeder::class);
         User::factory(100)->create();
+        Reservation::factory(100)->create();
         Review::factory(100)->create();
         $this->call(RolesAndPermissionsSeeder::class);
     }
