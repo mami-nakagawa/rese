19485aa1d27e77eea52dcd36556a3945e3c121ff
中川真美 <mya.a6a@gmail.com>
2024-08-19 21:21:59 +0900

    不要箇所削除

--- src/resources/views/qrcode-data.blade.php
+++ src/resources/views/qrcode-data.blade.php
@@ -13,15 +13,13 @@
 
 <body>
     <main>
-        <div class="qrcode-data__container">
-            <div class="qrcode-data">
-                <p>店舗名: {{ $reservation->shop->name }}</p><br>
-                <p>予約ID: {{ $reservation->id }}</p>
-                <p>予約名: {{ $reservation->user->name }}様</p>
-                <p>予約日: {{ $reservation->date }}</p>
-                <p>予約時間: {{ substr($reservation->time, 0, 5) }}</p>
-                <p>予約人数: {{ $reservation->number }}人</p>
-            </div>
+        <div class="qrcode-data">
+            <p>店舗名: {{ $reservation->shop->name }}</p><br>
+            <p>予約ID: {{ $reservation->id }}</p>
+            <p>予約名: {{ $reservation->user->name }}様</p>
+            <p>予約日: {{ $reservation->date }}</p>
+            <p>予約時間: {{ substr($reservation->time, 0, 5) }}</p>
+            <p>予約人数: {{ $reservation->number }}人</p>
         </div>
     </main>
 </body>
