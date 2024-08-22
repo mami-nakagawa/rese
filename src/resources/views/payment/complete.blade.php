e77526aa6d927670fbbe80695f8da391c4c54a9c
中川真美 <mya.a6a@gmail.com>
2024-08-22 22:16:38 +0900

    決済完了画面追加

--- src/resources/views/payment/complete.blade.php
+++ src/resources/views/payment/complete.blade.php
@@ -1,1 +1,14 @@
-この度はご購入いただき、誠にありがとうございます！
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/done.css')}}">
+@endsection
+
+@section('content')
+<div class="done__inner">
+  <div class="done__card">
+    <p class="done__message">決済が完了しました。</p>
+    <a class="back__btn" href="/">戻る</a>
+  </div>
+</div>
+@endsection
