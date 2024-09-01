7481f488c3ecf21e10cf5540a797ea5cd344c327
中川真美 <mya.a6a@gmail.com>
2024-09-01 12:13:55 +0900

    AWS S3インストール

--- src/app/Http/Controllers/EditorController.php
+++ src/app/Http/Controllers/EditorController.php
@@ -12,6 +12,7 @@ use Auth;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class EditorController extends Controller
 {
