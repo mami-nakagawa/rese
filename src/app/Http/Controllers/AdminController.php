b8f0a02979270b4aaeae9e5745f2a33c9d393312
中川真美 <mya.a6a@gmail.com>
2024-09-30 00:01:25 +0900

    管理者用新規店舗情報登録機能の追加

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Shop;
+use App\Models\Area;
+use App\Models\Genre;
 use App\Models\ShopRepresentative;
 use App\Mail\NotificationMail;
 use Mail;
@@ -11,7 +13,13 @@ use Spatie\Permission\Models\Role;
 use Auth;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use App\Http\Requests\ShopRepresentativeRequest;
 use Illuminate\Support\Facades\DB;
+use Exception;
+use App\Services\csvValidationService;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
+use Illuminate\Support\ViewErrorBag;
 
 class AdminController extends Controller
 {
@@ -22,7 +30,7 @@ class AdminController extends Controller
         return view('admin.admin', compact('user'));
     }
 
-    public function register(Request $request)
+    public function register(ShopRepresentativeRequest $request)
     {
         $password = $request->password;
 
@@ -50,5 +58,57 @@ class AdminController extends Controller
         Mail::to($users)->send(new NotificationMail($text));
 
         return redirect()->back()->with('message','お知らせメールを送信しました');
+    }
+
+    public function csvImport(Request $request)
+    {
+        if ($request->hasFile('csvFile')) {
+            // リクエストからファイルを取得
+            $file = $request->file('csvFile');
+            $path = $file->getRealPath();
+            // ファイルを開く
+            $fp = fopen($path, 'r');
+            // ヘッダー行をスキップ
+            fgetcsv($fp);
+            // 1行ずつ読み込む
+            while (($csvData = fgetcsv($fp)) !== FALSE) {
+                $validateErrors = csvValidationService::validateCsvDate($csvData);
+                if (!is_null($validateErrors)) {
+                    // バリデーションの エラーメッセージをセット
+                    session()->flash('errors', $validateErrors);
+                    // ファイルを閉じる
+                    fclose($fp);
+
+                    return redirect()->back();
+                } else {
+                    $this->InsertCsvData($csvData);
+                    // ファイルを閉じる
+                    fclose($fp);
+
+                    return redirect()->back()->with('shop_create_message','店舗情報を登録しました');
+                }
+            }
+        } else {
+            try {
+                throw new Exception();
+            } catch (Exception $e) {
+                return redirect()->back()->with('shop_create_message','CSVファイルの取得に失敗しました');
+            }
+        }
+    }
+
+    public function InsertCsvData($csvData)
+    {
+        // csvファイル情報をインサートする
+        $area = Area::where('name', $csvData[1])->first();
+        $genre = Genre::where('name', $csvData[2])->first();
+
+        $shop = new Shop;
+        $shop->name = $csvData[0];
+        $shop->area_id = $area->id;
+        $shop->genre_id = $genre->id;
+        $shop->summary = $csvData[3];
+        $shop->image = $csvData[4];
+        $shop->save();
     }
 }
